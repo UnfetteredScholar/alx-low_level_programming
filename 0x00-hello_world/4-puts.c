355a2fe60e7d20ce95ff6c29b8a73bbbf3821165
Ato Toffah <atotoffah@gmail.com>
2023-03-16 17:11:59 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Prints text using puts() */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
