90018b674899077f3a88ca9b9a358fb57ae1af16
Ato Toffah <atotoffah@gmail.com>
2023-03-21 17:10:52 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints all minutes in 24 hours
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int min = 0;
+	char time[4];
+
+	while (hours != 23 || min != 59)
+	{
+		min++;
+
+		if (min == 60)
+		{
+			hours++;
+			min = 0;
+		}
+		time[0] = '0' + ((hours / 10) % 10);
+		time[1] = '0' + (hours % 10);
+		time[2] = '0' + ((min / 10) % 10);
+		time[3] = '0' + (min % 10);
+
+		_putchar(time[0]);
+		_putchar(time[1]);
+		_pitchar(':');
+		_putchar(time[2]);
+		_putchar(time[3]);
+		_putchar('\n');
+	}
+}
