1bc765b7a928ac7cb1f3ec3c417a42c54698ada0
Ato Toffah <atotoffah@gmail.com>
2023-03-21 18:10:43 -0700

    Added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int nums[]= {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int r;
+
+	while (1)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (nums[i] > 9)
+				_putchar('0' + ((nums[i] / 10) % 10));
+			_putchar('0' + (nums[i] % 10));
+
+			if(nums[i] == 81)
+				return
+			_putchar(',');
+			_putchar(' ');
+			nums[i] += i;
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,3 +20,5 @@ void jack_bauer(void);
 int add(int, int);
 
 void print_to_98(int n);
+
+void times_table(void);
