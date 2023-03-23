de0c9e9ef984d9f65da5bcac37dce3270fa42a3a
Ato Toffah <atotoffah@gmail.com>
2023-03-23 15:43:42 -0700

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 exccept for 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+			continue;
+		_putchar(num);
+	}
+	_putchar('\n');
+}
