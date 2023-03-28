0435cf9e0ae9ea9fa6e2753e7c2f6713d1566a6a
Ato Toffah <atotoffah@gmail.com>
2023-03-27 17:18:57 -0700

    added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string from start
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+}
