1f7d993f9624f691dc7b07db0e82cfc8aedeecc6
Ato Toffah <atotoffah@gmail.com>
2023-04-05 17:57:13 -0700

    Fix 11 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,7 +9,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 1)
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
 		return (x);
 	return (x * _pow_recursion(x, y - 1));
 }
