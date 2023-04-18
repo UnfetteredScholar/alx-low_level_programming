101b15f8975961eed1691ae8b4e12ebc6eaaae3e
UnfetteredScholar <atotoffah@gmail.com>
2023-04-18 02:14:32 +0000

    Added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - descibes a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the name of the owner
+ *
+ * Description: structure for a dog object
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
