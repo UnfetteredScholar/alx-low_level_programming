26a088f301e6ab63d33676f6a55b435810879288
UnfetteredScholar <atotoffah@gmail.com>
2023-04-21 01:27:51 +0000

    Fix 2

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,10 +33,9 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				s = va_arg(args, char *);
-				if (s != NULL)
-					printf("%s", s);
-				else
-					printf("(nil)");
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
 				break;
 			default:
 				valid = 0;
