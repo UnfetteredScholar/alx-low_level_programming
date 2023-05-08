f8a257bbc11034d5850fbb46272f75334cd41f17
Ato Toffah <atotoffah@gmail.com>
2023-05-08 23:09:23 +0000

    Fix 1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,6 +44,34 @@ int write_to_file(int file, char *buff, int letters)
 }
 
 /**
+ * _frw_error - prints file read or write error
+ * @f: name of the file
+ * @code: the exit code
+ *
+ * Return: void
+ */
+void _frw_error(char *f, int code)
+{
+	if (code == 98)
+		dprintf(2, "Error: Can't read from file %s\n", f);
+	else if (code == 99)
+		dprintf(2, "Error: Can't write tp %s\n", f);
+	exit(code);
+}
+
+/**
+ * _fclose_error - prints fcolse error
+ * @fd: the file descriptor
+ *
+ * Return: void
+ */
+void _fclose_error(int fd)
+{
+	dprintf(2, "Error: Can't close fd %d\n", fd);
+	close(100);
+}
+
+/**
  * main - copies content form one file to another
  * @argv: array of arguments
  * @argc: the number of arguments
@@ -52,7 +80,7 @@ int write_to_file(int file, char *buff, int letters)
  */
 int main(int argc, char *argv[])
 {
-	char *buff = NULL;
+	char *buff = malloc(sizeof(char) * 1024);
 	int count = 0, len = 0, fr = 0, fw = 0;
 
 	if (argc != 3)
@@ -60,47 +88,28 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buff = malloc(sizeof(char) * 1024);
 	if (buff == NULL)
 		return (0);
 	fr = open(argv[1], O_RDONLY);
 	fw = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fr < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		_frw_error(argv[1], 98);
 	if (fw < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		_frw_error(argv[2], 99);
 	while (1)
 	{
 		count = read_to_buffer(fr, buff, 1024);
 		if (count < 0)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			_frw_error(argv[1], 98);
 		if (count == 0)
 			break;
 		len = write_to_file(fw, buff, count);
 		if (len == -1 || len != count)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			_frw_error(argv[2], 99);
 	}
 	if (close(fr) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fr);
-		exit(100);
-	}
+		_fclose_error(fr);
 	if (close(fw) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fw);
-		exit(100);
-	}
+		_fclose_error(fw);
 	return (0);
 }
