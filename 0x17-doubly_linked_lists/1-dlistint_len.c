01af785f4b0f9738cdc2562cd85084bfea5e3f65
Ato Toffah <atotoffah@gmail.com>
2023-08-17 22:49:08 +0000

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lists.h>
+
+/**
+ * dlistint_len - finds the length of a dlist
+ * @h: pointer to the head of the linked list
+ *
+ * Return: the length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
