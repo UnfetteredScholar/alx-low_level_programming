7c5de3c9fe07c6ff6b0d5f0975b333b5a8dc7a65
Ato Toffah <atotoffah@gmail.com>
2023-09-08 17:52:28 +0000

    Fix 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,6 +17,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	size--;
+	while (size > 0)
+		table->array[size--] = NULL;
 
 	return (table);
 }
